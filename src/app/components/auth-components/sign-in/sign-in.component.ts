f02a31901250952e0a91d90ac3b376e6efd4aa49
Oleksandr Panchuk <olexandrpan4uk@gmail.com>
2024-03-09 00:16:54 +0200

    sign up form updated mutual classes are decomposed to auth-style.css

--- src/app/components/auth-components/sign-in/sign-in.component.ts
+++ src/app/components/auth-components/sign-in/sign-in.component.ts
@@ -17,7 +17,6 @@ import {TokensDto} from "../../../common/dto/tokens.dto";
 export class SignInComponent implements AfterViewInit {
   @ViewChildren('inputField') inputFields!: QueryList<ElementRef>;
 
-
   loginForm = this.formBuilder.group({
     username: ['', [Validators.required]],
     password: ['', [Validators.required]],
@@ -36,9 +35,9 @@ export class SignInComponent implements AfterViewInit {
     this.inputFields.forEach(inputField => {
       inputField.nativeElement.addEventListener('blur', () => {
         if (inputField.nativeElement.value != "") {
-          inputField.nativeElement.classList.add('has-val');
+          inputField.nativeElement.classList.add('auth__has-value');
         } else {
-          inputField.nativeElement.classList.remove('has-val');
+          inputField.nativeElement.classList.remove('auth__has-value');
         }
       });
     });
@@ -54,8 +53,6 @@ export class SignInComponent implements AfterViewInit {
           next: (data: any) => {
 
             this.storageService.saveTokens(data)
-            // localStorage.setItem('accessToken', data.accessToken);
-            // localStorage.setItem('refreshToken', data.refreshToken);
 
             // Navigate to UserAccount component upon successful login
             this.router.navigate(['/user-profile']);
--- src/app/components/auth-components/sign-up/sign-up.component.ts
+++ src/app/components/auth-components/sign-up/sign-up.component.ts
@@ -1,25 +1,72 @@
-import { Component } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faGoogle, faGithub, faFacebook } from '@fortawesome/free-brands-svg-icons';
-import { RouterLink } from '@angular/router';
+import {AfterViewInit, Component, ElementRef, QueryList, ViewChildren} from '@angular/core';
+import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {faFacebook, faGithub, faGoogle} from '@fortawesome/free-brands-svg-icons';
+import {Router, RouterLink} from '@angular/router';
+import {FormBuilder, FormsModule, ReactiveFormsModule, Validators} from "@angular/forms";
+import {StorageService} from "../../../services/storage.service";
+import {AuthService} from "../../../services/auth.service";
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [FontAwesomeModule, RouterLink],
+  imports: [FontAwesomeModule, RouterLink, FormsModule, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
-export class SignUpComponent {
+export class SignUpComponent implements AfterViewInit {
+  @ViewChildren('inputField') inputFields!: QueryList<ElementRef>;
 
-  faGoogle = faGoogle;
-  faGithub = faGithub;
-  faFacebook = faFacebook;
+  registerForm = this.formBuilder.group({
+    username: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
-    
+  constructor(
+    private formBuilder: FormBuilder,
+    private storageService: StorageService,
+    private authService: AuthService,
+    private router: Router
+  ) {
   }
 
+  ngAfterViewInit() {
+
+    this.inputFields.forEach(inputField => {
+      inputField.nativeElement.addEventListener('blur', () => {
+        if (inputField.nativeElement.value != "") {
+          inputField.nativeElement.classList.add('auth__has-value');
+        } else {
+          inputField.nativeElement.classList.remove('auth__has-value');
+        }
+      });
+    });
+
+  }
+
+  onSubmitRegister() {
+    if (this.registerForm.valid) {
+      this.authService.register(
+        this.registerForm.getRawValue().username!,
+        this.registerForm.getRawValue().password!,
+        this.registerForm.getRawValue().email!)
+        .subscribe({
+          next: (data: any) => {
+
+            this.storageService.saveTokens(data)
+
+            // Navigate to UserAccount component upon successful registration
+            this.router.navigate(['/user-profile']);
+          },
+          error: (err: any) => {
+            // Display an error message
+            console.error('An error occurred:', err);
+          }
+        });
+    }
+  }
+
+  protected readonly faGoogle = faGoogle;
+  protected readonly faGithub = faGithub;
+  protected readonly faFacebook = faFacebook;
 }
